7a61958bb1abe3c39b46c1c59848e0cb79db36ce
Clara McKenzie <clara@hashicorp.com>
2020-10-02 14:10:58 -0700

    add main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "production-resources"
+  location = "West US"
+}
+
+# Create a virtual network in the production-resources resource group
+resource "azurerm_virtual_network" "test" {
+  name                = "production-network"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = "${azurerm_resource_group.example.location}"
+  address_space       = ["10.0.0.0/16"]
+}
